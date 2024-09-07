ac05971e48fdd34cfd6879a844cdd37d453efc5c
= <=>
2024-09-07 16:23:50 -0700

    first upload

---
+++ main.js
@@ -0,0 +1,57 @@
+
+
+
+
+
+
+fetch('https://regres.in/api/users')
+.then(res => res.json() )
+.then(data => handle(data));
+
+
+function handle(inputData) {
+
+    const headers = Obect.keys(inputData.data[0]).toString();
+    console.log(headers);
+
+    const main = inputData.data.map(item => {
+
+        return Object.values(item).toString();
+
+    });
+
+    console.log(main);
+
+    const csv = [headers, ...main].join('\n');
+    console.log(csv);
+
+
+}
+
+
+function startCSVDownload() {
+
+    const blob = new Blob([input], { type: 'application/csv' } );
+
+    const url= URL.createObjectURL(blob);
+
+
+    document.getElementById('btn').addEventListener('click', () => {
+
+        const a = document.createElement('a');
+        a.download = 'test-csv.csv';
+        a.href = url;
+        a.style.display = 'none';
+    
+        document.body.appendChild(a);
+    
+        a.click();
+        a.remove();
+        URL.revokeObjectURL(url);
+
+    });
+
+
+
+
+}
