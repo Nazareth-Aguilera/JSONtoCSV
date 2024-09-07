a2241e2a485ddaa4d35f898c7651bdc891fbad65
= <=>
2024-09-07 16:54:53 -0700

    scoping changes

--- main.js
+++ main.js
@@ -1,72 +1,74 @@
 
+(function() {
 
-
-console.log("Application starting");
-try {
-    fetch('https://www.reddit.com/r/latinas.json')
-    .then(res => res.json() )
-    .then(data => {
-        console.log("getting data");
-        console.log(data);
-        handle(data)
-
-    });
-} catch(error) {
-    console.log(error);
-}
-
-
-console.log("---------------------------------------------");
-
-
-
-
-function handle(inputData) {
-
-    const headers = Object.keys(inputData.data[0]).toString();
-    console.log(headers);
-
-    const main = inputData.data.map(item => {
-
-        return Object.values(item).toString();
-
-    });
-
-    console.log(main);
-
-    const csv = [headers, ...main].join('\n');
-    console.log(csv);
-
-    startCSVDownload(csv);
-
-
-
-}
-
-
-function startCSVDownload(input) {
-
-    const blob = new Blob([input], { type: 'application/csv' } );
-
-    const url= URL.createObjectURL(blob);
-
-
-    document.getElementById('btn').addEventListener('click', () => {
-
-        const a = document.createElement('a');
-        a.download = 'test-csv.csv';
-        a.href = url;
-        a.style.display = 'none';
+    console.log("Application starting");
+    try {
+        fetch('https://www.reddit.com/r/latinas.json')
+        .then(res => res.json() )
+        .then(data => {
+            console.log("getting data");
+            console.log(data);
+            handle(data)
     
-        document.body.appendChild(a);
+        });
+    } catch(error) {
+        console.log(error);
+    }
     
-        a.click();
-        a.remove();
-        URL.revokeObjectURL(url);
+    
+    console.log("---------------------------------------------");
+    
+    
+    
+    
+    function handle(inputData) {
+    
+        const headers = Object.keys(inputData.data[0]).toString();
+        console.log(headers);
+    
+        const main = inputData.data.map(item => {
+    
+            return Object.values(item).toString();
+    
+        });
+    
+        console.log(main);
+    
+        const csv = [headers, ...main].join('\n');
+        console.log(csv);
+    
+        startCSVDownload(csv);
+    
+    
+    
+    }
+    
+    
+    function startCSVDownload(input) {
+    
+        const blob = new Blob([input], { type: 'application/csv' } );
+    
+        const url= URL.createObjectURL(blob);
+    
+    
+        document.getElementById('btn').addEventListener('click', () => {
+    
+            const a = document.createElement('a');
+            a.download = 'test-csv.csv';
+            a.href = url;
+            a.style.display = 'none';
+        
+            document.body.appendChild(a);
+        
+            a.click();
+            a.remove();
+            URL.revokeObjectURL(url);
+    
+        });
+    
+    }
 
-    });
 
 
+})();
 
-
-}
